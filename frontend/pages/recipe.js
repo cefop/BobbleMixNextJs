55a29d1dd4a1fada53b00e851234a799f7d91b6e
Remi Roubin <robyremzy@disroot.org>
2021-07-15 08:46:58 +0200

    add recipe

--- frontend/pages/recipe.js
+++ frontend/pages/recipe.js
@@ -1,5 +1,5 @@
 import PageLayout from '../components/styles/PageLayout';
-import UserRecipe from '../components/UserRecipe/index';
+import UserRecipe from '../components/Recipe/index';
 
 export default function Recipe(props) {
     return (
