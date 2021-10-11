afa0f040413afc783b65a26f1edf190f85c7fcb3
Remi Roubin <robyremzy@disroot.org>
2021-10-11 19:10:43 +0200

    Fix bug #12 -- translate email

--- frontend/pages/api/auth/[...nextauth].js
+++ frontend/pages/api/auth/[...nextauth].js
@@ -1,6 +1,8 @@
 import jwt from 'jsonwebtoken';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import nodemailer from 'nodemailer';
+// import SendVerificationRequest from '../../../components/mail/MailerLogin';
 
 export default NextAuth({
     // Configure one or more authentication providers
@@ -31,7 +33,70 @@ export default NextAuth({
                 },
             },
             from: process.env.EMAIL_FROM,
-            // TODO https://next-auth.js.org/providers/email#customizing-emails
+            // ? custom email template
+            sendVerificationRequest({ identifier: email, url, provider: { server, from } }) {
+                const { host } = new URL(url);
+                const transport = nodemailer.createTransport(server);
+
+                // Email HTML body
+                function html({ url, host, email }) {
+                    // Insert invisible space into domains and email address to prevent both the
+                    // email address and the domain from being turned into a hyperlink by email
+                    // clients like Outlook and Apple mail, as this is confusing because it seems
+                    // like they are supposed to click on their email address to sign in.
+                    const escapedEmail = `${email.replace(/\./g, '&#8203;.')}`;
+                    const escapedHost = `${host.replace(/\./g, '&#8203;.')}`;
+
+                    // Some simple styling options
+                    const backgroundColor = '#f9f9f9';
+                    const textColor = '#444444';
+                    const mainBackgroundColor = '#ffffff';
+                    const buttonBackgroundColor = '#346df1';
+                    const buttonBorderColor = '#346df1';
+                    const buttonTextColor = '#ffffff';
+
+                    return `
+                        <body style="background: ${backgroundColor};">
+                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                            <tr>
+                                <td align="center" style="padding: 10px 0px 20px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+                                <strong>${escapedHost}</strong>
+                                </td>
+                            </tr>
+                            </table>
+                            <table width="100%" border="0" cellspacing="20" cellpadding="0" style="background: ${mainBackgroundColor}; max-width: 600px; margin: auto; border-radius: 10px;">
+                            <tr>
+                                <td align="center" style="padding: 10px 0px 0px 0px; font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+                                Enregistez-vous avec: <strong>${escapedEmail}</strong>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="padding: 20px 0;">
+                                <table border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                    <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Connection</a></td>
+                                    </tr>
+                                </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td align="center" style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${textColor};">
+                                Si vous n'avez pas demandé cet e-mail, vous pouvez l'ignorer en toute sécurité.
+                                </td>
+                            </tr>
+                            </table>
+                        </body>
+                        `;
+                }
+
+                transport.sendMail({
+                    to: email,
+                    from,
+                    subject: `👋 Bienvenue sur Bobblemix`,
+                    text: `Votre lien de login, \n Cliqué sur ce lien ${url} pour vous enregistrer sur ${host} \n\n`,
+                    html: html({ url, host, email }),
+                });
+            },
         }),
         // ...add more providers here
     ],
